db4c13dbe605d61f9b3959f34d70a4f37edf42c6
jinyicheng <jinyicheng@live.com>
2021-03-11 17:20:10 +0800

    增加配置自动生成

---
+++ src/ClientProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * COPClient
+ * PHP version 7
+ *
+ * @category Class
+ * @package  COP\Client
+ * @author   jinyicheng
+ * @link     https://github.com/jinyicheng/cop
+ */
+
+namespace jinyicheng\cop;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class Client
+ * @package jinyicheng\cop
+ * @author   jinyicheng
+ * @link     https://github.com/jinyicheng/cop
+ */
+class ClientProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 发布配置文件
+        $this->publishes([
+            __DIR__.'/config/cop.php' => config_path('cop.php'),
+        ]);
+    }
+}
